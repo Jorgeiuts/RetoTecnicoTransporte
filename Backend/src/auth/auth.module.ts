1c0568e4f2e3bbc5c09c5f0160429af63a2b1a9f
Jorge Alonso Castañeda Sotelo <jorge.alonso241099@gmail.com>
2026-09-05 16:59:31 -0600

    feat(auth): configurar módulo de autenticación con JWT

---
+++ Backend/src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from "@nestjs/common";
+import { UsersModule } from "../users/users.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule, JwtSignOptions } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./jwt.strategy";
+import { AuthController } from "./auth.controller";
+
+@Module({
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                secret: config.get<string>('JWT_SECRET'),
+                signOptions: {
+                    expiresIn: config.get<JwtSignOptions['expiresIn']>('JWT_EXPIRES_IN') || '1d',
+                },
+            }),
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy],
+    exports: [AuthService]
+})
+export class AuthModule {}
