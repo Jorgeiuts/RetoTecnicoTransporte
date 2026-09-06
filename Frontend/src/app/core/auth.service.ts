79af66254f40ebf19db365686c82d0c12f47406e
Jorge Alonso Castañeda Sotelo <jorge.alonso241099@gmail.com>
2026-09-06 10:14:36 -0600

    feat(frontend-auth): agregar servicio de autenticación

---
+++ Frontend/src/app/core/auth.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable, signal } from "@angular/core";
+import { Router } from "@angular/router";
+import { environment } from '../../environments/environment';
+import { tap } from "rxjs";
+
+@Injectable({ providedIn: 'root'})
+export class AuthService {
+  private readonly tokenKey = 'access_token';
+  readonly isLoggedIn = signal(!!localStorage.getItem(this.tokenKey));
+
+  constructor(
+    private readonly http: HttpClient,
+    private readonly router: Router,
+  ) {}
+
+  iniciarSesion(email: string, password: string) {
+    return this.http
+    .post<{ access_token: string }>(`${environment.apiUrl}/auth/iniciar-sesion`, {
+      email,
+      password,
+    })
+    .pipe(
+      tap((respuesta) =>{
+        localStorage.setItem(this.tokenKey, respuesta.access_token);
+        this.isLoggedIn.set(true);
+      })
+    );
+  }
+
+  cerrarSesion() {
+    this.http.post(`${environment.apiUrl}/auth/cerrar-sesion`, {}).subscribe({
+      error: () => undefined,
+    });
+    this.isLoggedIn.set(false);
+    this.router.navigateByUrl('/auth/iniciar-sesion');
+  }
+
+  obtenerToken() {
+    return localStorage.getItem(this.tokenKey);
+  }
+}
